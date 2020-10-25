a5edfa78fd536df764431803f7114de6708ddf32
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-10-25 09:11:11 -0600

    added model table values

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        //validates value is a decimal
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Set a default value of 10
+      validate: {
+        //validates that value is numeric
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // references the category model's id
+    }
   },
   {
     sequelize,
